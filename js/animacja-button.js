457acad25c3c4cc7076c7fcb2c693d73e1d3b6b2
Marcin Mika <marcinmika@hotmail.com>
2017-07-22 08:46:33 +0200

    do poprawy
    zrobic powrót do widoku z przed kliknieciu button
    ustawic linki na experiance icon by otwieraly opis

--- js/animacja-button.js
+++ js/animacja-button.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     'use strict';
-    $("button").click(function(){
+    $("button").click(function(){	
         $("p").slideToggle(1000,'linear',function(){
 			$('form').css('display','none');
 			$('h3').css('display','none');
 			$('.experiance-icon').css('display','none');
-		});
+			});
     });
 });
