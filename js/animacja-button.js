e06df72cac84c5300d85efc83e2935e8de51a081
Marcin Mika <marcinmika@hotmail.com>
2017-07-25 10:10:27 +0200

    popraw animacja-button.js
    błąd w kodzie powodował, że aniamacja się nei wykonywała

--- js/animacja-button.js
+++ js/animacja-button.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
-    'use strict';
+'use strict';
     $("button").click(function(){	
         $("p").slideToggle(1000,'linear',function(){
 			$('form').css('display','none');
 			$('h3').css('display','none');
-//			$('.experiance-icon').css('display','none');
+			$('.experiance-icon').css('display','none');
 			});
     });
 	
